fa88e4c0c98cdbabc42421227298540457527884
sapars <110113627+sapars@users.noreply.github.com>
2024-09-09 16:40:28 +0300

    Update module_12_1.py

--- module_12_3/12_3_No skip/module_12_1.py
+++ module_12_3/12_3_No skip/module_12_1.py
@@ -33,28 +33,28 @@ import unittest
 
 
 class RunnerTest(unittest.TestCase):
-    is_frozen = False
+    #is_frozen = False
 
     @classmethod
     def setUpClass(cls):
         print('RunnerTest')
         print('==========')
 
-    @unittest.skipIf(is_frozen, 'Тесты в этом кейсе заморожены')
+    #@unittest.skipIf(is_frozen, 'Тесты в этом кейсе заморожены')
     def test_walk(self):
         runner = Runner('Test')
         for i in range(10):
             runner.walk()
         self.assertEqual(runner.distance, 50)
 
-    @unittest.skipIf(is_frozen, 'Тесты в этом кейсе заморожены')
+    #@unittest.skipIf(is_frozen, 'Тесты в этом кейсе заморожены')
     def test_run(self):
         runner = Runner('Test')
         for i in range(10):
             runner.run()
         self.assertEqual(runner.distance, 100)
 
-    @unittest.skipIf(is_frozen, 'Тесты в этом кейсе заморожены')
+    #@unittest.skipIf(is_frozen, 'Тесты в этом кейсе заморожены')
     def test_challenge(self):
         runner1 = Runner('Test1')
         runner2 = Runner('Test2')
